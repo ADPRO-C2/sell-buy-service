81863c88e27b08d4108b759614097ed3515d9c23
syifakaffa <syifa.kaffa@ui.ac.id>
2024-04-24 10:30:12 +0700

    [RED] Membuat testEditListing di service

--- src/test/java/com/example/secondtreasurebe/service/ListingServiceTest.java
+++ src/test/java/com/example/secondtreasurebe/service/ListingServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.secondtreasurebe.service;
 
 import com.example.secondtreasurebe.model.Listing;
+import com.example.secondtreasurebe.model.UsedListing;
 import com.example.secondtreasurebe.model.ListingRequest;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -119,5 +120,54 @@ public class ListingServiceTest {
 
         Listing foundListing = service.findListingById("non-existent-id");
         assertNull(foundListing);
+    }
+
+    @Test
+    void testEditListing() {
+        ListingRequest request1 = new ListingRequest("eb558e9f-1c39-460e-8860-71af6af63ba7");
+        request1.setListingId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        request1.setName("Kemeja Linen Blend");
+        request1.setStock(10);
+        request1.setDescription("Kerah terbuka, bahan nyaman dipakai.");
+        request1.setPhotoUrl("https://image.uniqlo.com/UQ/ST3/id/imagesgoods/467247/item/idgoods_09_467247.jpg?width=750");
+        request1.setPrice(299000);
+        request1.setRateCondition(0);
+        Listing listing1 = service.createListing(request1);
+        System.out.println(listing1.getName());
+
+        Listing listing2 = new UsedListing();
+        listing2.setUserId("eb558e9f-1c39-460e-8860-71af6af63ba7");
+        listing2.setListingId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        listing2.setName("T-Shirt Kerah Bulat");
+        listing2.setStock(50);
+        listing2.setDescription("Enak dipakai");
+        listing2.setPhotoUrl("https://image.uniqlo.com/UQ/ST3/id/imagesgoods/424873/item/idgoods_08_424873.jpg?width=320");
+        listing2.setPrice(149000);
+        listing2.setRateCondition(2);
+        System.out.println(listing2.getName());
+
+        service.edit(listing2);
+
+        Listing edited = service.findListingById(listing1.getListingId());
+
+        // Verifikasi bahwa produk telah diubah dengan benar
+        assertEquals(listing2.getListingId(), edited.getListingId());
+        assertEquals(listing2.getName(), edited.getName());
+        assertEquals(listing2.getUserId(), edited.getUserId());
+        assertEquals(listing2.getStock(), edited.getStock());
+        assertEquals(listing2.getDescription(), edited.getDescription());
+        assertEquals(listing2.getPhotoUrl(), edited.getPhotoUrl());
+        assertEquals(listing2.getPrice(), edited.getPrice());
+        assertEquals(listing2.getRateCondition(), edited.getRateCondition());
+
+        // Verifikasi bahwa data produk yang disimpan telah diperbarui
+        assertEquals(listing2.getListingId(), listing1.getListingId());
+        assertEquals(listing2.getName(), listing1.getName());
+        assertEquals(listing2.getUserId(), listing1.getUserId());
+        assertEquals(listing2.getStock(), listing1.getStock());
+        assertEquals(listing2.getDescription(), listing1.getDescription());
+        assertEquals(listing2.getPhotoUrl(), listing1.getPhotoUrl());
+        assertEquals(listing2.getPrice(), listing1.getPrice());
+        assertEquals(listing2.getRateCondition(), listing1.getRateCondition());
     }
 }
