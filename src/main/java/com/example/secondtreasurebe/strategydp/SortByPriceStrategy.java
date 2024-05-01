c6f8d1bad9a45492f996bf976f1c732a30f0c61f
syifakaffa <syifa.kaffa@ui.ac.id>
2024-05-01 15:17:07 +0700

    [GREEN] Implemen SortByPriceStrategy

---
+++ src/main/java/com/example/secondtreasurebe/strategydp/SortByPriceStrategy.java
@@ -0,0 +1,15 @@
+package com.example.secondtreasurebe.strategydp;
+
+import com.example.secondtreasurebe.model.Listing;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortByPriceStrategy implements SortStrategy {
+    @Override
+    public List<Listing> sort(List<Listing> listings) {
+        Collections.sort(listings, Comparator.comparingInt(Listing::getPrice));
+        return listings;
+    }
+}
