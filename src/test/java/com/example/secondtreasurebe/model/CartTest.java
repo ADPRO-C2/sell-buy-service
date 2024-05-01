eae95df8fa997f038218850fd5764876a039443c
jteo0 <jjtheodora@gmail.com>
2024-05-01 10:44:38 +0700

    [RED] Fixing Cart model test.

--- src/test/java/com/example/secondtreasurebe/model/CartTest.java
+++ src/test/java/com/example/secondtreasurebe/model/CartTest.java
@@ -8,20 +8,19 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CartTest {
-    // private List<CartListing> cartListings;
 
     @BeforeEach
     void setUp() {
-        this.cartListings = new ArrayList<>();
         Cart testCart = new Cart();
-        CartListing cartListing1 = new CartListing("11", 2);
-        CartListing cartLisitng2 = new CartListing("22", 3);
+        testCart.setUserId("eb558e9f-1c39-460e-8860-71af6af63ba7");
     }
 
     @Test
-    void testCreateEmptyCart() {
-        Cart cart = new Cart("13652556-012a-4c07-b56-54eb1396d79b");
-        cart.setCartId("eb558e9f-1c39-460e-8860-71af6af63ba7"); //set this one
+    void testEmptyCart() {
+        Cart cart = new Cart();
+        List<CartListing> empty = new ArrayList<>();
+        cart.setUserId("13652556-012a-4c07-b56-54eb1396d79b");
+        cart.setItems(empty);
 
         assertEquals(cart.getUserId(), "13652556-012a-4c07-b56-54eb1396d79b");
         assertEquals(cart.getCartId(), "eb558e9f-1c39-460e-8860-71af6af63ba7");
@@ -34,10 +33,13 @@ class CartTest {
 
     @Test
     void testListingsInCart() {
-
-        List<Listing> listingTest = new ArrayList<>();
+        List<CartListing> items = new ArrayList();
         Listing listing1 = new Listing();
-        listingTest.add(listing1);
-        assertEquals(this.carts.get(1).getInCart(), listingTest);
+        Listing listing2 = new Listing();
+        CartListing cartListing1 = new CartListing(listing1, 2);
+        CartListing cartListing2 = new CartListing(listing2, 3);
+        items.add(cartListing1);
+        items.add(cartListing2);
+        testCart.setItems(items);
     }
 }
