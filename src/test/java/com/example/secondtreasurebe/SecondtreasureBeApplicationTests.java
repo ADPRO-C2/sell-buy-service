835fa6cc8d1ee5b8b10c6bdb1e69650d7ddb7ab2
syifakaffa <syifa.kaffa@ui.ac.id>
2024-05-12 13:21:33 +0700

    [REFACTOR] Build and Deployment

--- src/test/java/com/example/secondtreasurebe/SecondtreasureBeApplicationTests.java
+++ src/test/java/com/example/secondtreasurebe/SecondtreasureBeApplicationTests.java
@@ -4,9 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SecondtreasureBeApplicationTests {
-    @Test
-    void contextLoads() {
-    }
-
-}
+class SecondtreasureBeApplicationTests { }
