ab926c5923d04c7a90b63c03b92c7967a35cc76b
jteo0 <jjtheodora@gmail.com>
2024-05-13 17:35:11 +0700

    [REFACTOR] Deleting unused repository tests.

--- src/test/java/com/example/secondtreasurebe/repository/OrderRepositoryTest.java
+++ src/test/java/com/example/secondtreasurebe/repository/OrderRepositoryTest.java
@@ -1,140 +1,61 @@
 package com.example.secondtreasurebe.repository;
 
-import com.example.secondtreasurebe.model.Cart;
-import com.example.secondtreasurebe.model.Listing;
-import com.example.secondtreasurebe.model.CartListing;
 import com.example.secondtreasurebe.model.Order;
+import com.example.secondtreasurebe.model.CartListing;
+import com.example.secondtreasurebe.model.OrderStatus;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderRepositoryTest {
 
-    @InjectMocks
+    @Mock
     OrderRepository orderRepository;
 
-    List<Order> orderList = new ArrayList<>();
+    @Test
+    void testFindAllByUserId() {
+        String userId = "98726832-2f2e-45d4-bd67-6def918fdaa6";
+        List<Order> orders = new ArrayList<>();
+        orders.add(createOrder("77fcb080-a499-4e50-a610-c5fe49841f1b", userId));
+        orders.add(createOrder("5c0803ec-54aa-405c-8f46-460176365cf3", userId));
+        when(orderRepository.findAllByUserId(userId)).thenReturn(orders);
 
-    @BeforeEach
-    void setUp() {
-        Cart cart = new Cart("c56c2def-f1d7-4462-91c2-6f2dfaf08380");
-        List<CartListing> items = new ArrayList<>();
-        items.add(new CartListing.Builder()
-                .listing(new Listing())
-                .amount(1)
-                .build());
-        items.add(new CartListing.Builder()
-                .listing(new Listing())
-                .amount(2)
-                .build());
-        cart.setItems(items);
-        Order order = new Order(cart);
-        order.setOrderId("4a90b156-04a2-48b2-abb5-70976cb01d79");
-        orderList.add(order);
-    }
+        List<Order> foundOrders = orderRepository.findAllByUserId(userId);
 
-    @AfterEach
-    void tearDown() {
-        orderList.clear();
+        assertEquals(2, foundOrders.size());
+        assertEquals(userId, foundOrders.get(0).getUserId());
+        assertEquals(userId, foundOrders.get(1).getUserId());
     }
 
     @Test
-    void testSaveOrder() {
-        Order order = orderList.get(0);
-        orderRepository.save(order);
+    void testFindAllByUserIdNotFound() {
+        String userId = "0bfe6cb8-b193-4a83-bbee-83ef3e3e65fe";
+        when(orderRepository.findAllByUserId(userId)).thenReturn(new ArrayList<>());
 
-        Order findSavedOrder = orderRepository.findById(order.getOrderId());
-        assertEquals(order.getUserId(), findSavedOrder.getUserId());
-        assertEquals(order.getItems(), findSavedOrder.getItems());
+        List<Order> foundOrders = orderRepository.findAllByUserId(userId);
+
+        assertEquals(0, foundOrders.size());
     }
 
-    @Test
-    void testUpdateOrder() {
-        Order order = orderList.get(0);
-        orderRepository.save(order);
-
-        Order updatedOrder = orderRepository.findById(order.getOrderId());
-        List<CartListing> updatedItems = new ArrayList<>();
-        updatedItems.add(new CartListing.Builder()
-                .listing(new Listing())
-                .amount(3)
-                .build());
-        updatedOrder.setItems(updatedItems);
-
-        orderRepository.update(updatedOrder);
-        Order check = orderRepository.findById(order.getOrderId());
-        assertEquals(updatedOrder.getItems(), check.getItems());
-    }
-
-    @Test
-    void testDeleteOrder() {
-        Order order = orderList.get(0);
-        orderRepository.save(order);
-        Order findSavedOrder = orderRepository.findById(order.getOrderId());
-
-        assertNotNull(findSavedOrder);
-        orderRepository.delete(order.getOrderId());
-        findSavedOrder = orderRepository.findById(order.getOrderId());
-
-        assertNull(findSavedOrder);
-    }
-
-    @Test
-    void testFindById() {
-        Order order2 = new Order(new Cart("d19c7d53-a48e-485e-a824-e5a1c99d6e27"));
-        order2.setOrderId("b1c7c39f-1426-449e-a6fc-17db9ba7076e");
-        orderRepository.save(order2);
-
-        Order check = orderList.get(0);
-        orderRepository.save(check);
-        Order findOrder = orderRepository.findById(check.getOrderId());
-
-        assertEquals(check.getUserId(), findOrder.getUserId());
-        assertEquals(check.getItems(), findOrder.getItems());
-    }
-
-    @Test
-    void testFindAllOrders() {
-        Order order2 = new Order(new Cart("d19c7d53-a48e-485e-a824-e5a1c99d6e27"));
-        Order order3 = new Order(new Cart("50618969-f5a0-445a-b339-faea5c071b88"));
-        orderList.add(order2);
-        orderList.add(order3);
-
-        for (Order order : orderList) {
-            orderRepository.save(order);
-        }
-
-        List<Order> allOrders = orderRepository.findAll();
-        assertEquals(3, allOrders.size());
-    }
-
-    @Test
-    void testFindAllOrderFromUser() {
-        Order order1 = new Order(new Cart("61598f17-bfb8-4e15-a2d9-af6030be590a"));
-        Order order2 = new Order(new Cart("c8c3bc59-82ab-4ecc-8396-7a1e1d9c95a2"));
-        orderRepository.save(order1);
-        orderRepository.save(order2);
-
-        for (int i = 0; i < 3; i++) {
-            Order userOrder = new Order(new Cart("61598f17-bfb8-4e15-a2d9-af6030be590a"));
-            orderRepository.save(userOrder);
-        }
-
-        List<Order> userOrders = orderRepository.findAllUserOrders("61598f17-bfb8-4e15-a2d9-af6030be590a");
-
-        assertEquals(4, userOrders.size());
-
-        for (Order order : userOrders) {
-            assertEquals("61598f17-bfb8-4e15-a2d9-af6030be590a", order.getUserId());
-        }
+    private Order createOrder(String orderId, String userId) {
+        Order order = new Order();
+        order.setOrderId(orderId);
+        order.setUserId(userId);
+        order.setPriceTotal(100);
+        order.setStatus(OrderStatus.DIKEMAS);
+        return order;
     }
 }
