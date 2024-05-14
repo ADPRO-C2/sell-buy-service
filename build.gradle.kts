c16eefcb36c7e9099ba5aa7b663e3abd69e69990
syifakaffa <syifa.kaffa@ui.ac.id>
2024-05-15 05:14:50 +0700

    Fix SonarCloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,33 +49,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 }
 
-tasks.register<Test>("unitTest") {
-    description = "Runs unit tests."
-    group = "verification"
-
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-}
-
-tasks.register<Test>("functionalTest") {
-    description = "Runs functional tests."
-    group = "verification"
-
-    filter {
-        includeTestsMatching("*FunctionalTest")
-    }
-}
 
 tasks.withType<Test>().configureEach{
     useJUnitPlatform()
 }
 
 tasks.test {
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-
+    useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
 }
 
@@ -83,7 +63,9 @@ tasks.jacocoTestReport {
     dependsOn(tasks.test)
 
     reports {
-        html.required = true
         xml.required = true
+        csv.required.set(false)
+        html.required = true
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
     }
 }
