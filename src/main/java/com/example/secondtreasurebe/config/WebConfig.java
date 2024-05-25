fcb7ba4135973d4e3eaba7f6b5e3d270f85aaadb
syifakaffa <syifa.kaffa@ui.ac.id>
2024-05-25 12:03:29 +0700

    [REFACTOR] Mengubah Parameter untuk SortedListing

--- src/main/java/com/example/secondtreasurebe/config/WebConfig.java
+++ src/main/java/com/example/secondtreasurebe/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:3000")
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3001")
                 .allowedMethods("GET", "POST", "DELETE","PUT", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept")
                 .allowCredentials(true);
