744481f34b2318d85d7a4e503f4b0e0a2ca09eea
syifakaffa <syifa.kaffa@ui.ac.id>
2024-05-25 16:45:46 +0700

    coba postman deploy lagi

--- src/main/java/com/example/secondtreasurebe/config/WebConfig.java
+++ src/main/java/com/example/secondtreasurebe/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOriginPatterns("http://localhost:3001")
                 .allowedMethods("GET", "POST", "DELETE","PUT", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept")
